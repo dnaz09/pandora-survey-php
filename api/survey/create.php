bd401b859c62e00dd3f2f32ce9d2752e7a60b9d9
Daniel Naz <dnaz09@yahoo.com>
2019-07-12 15:03:13 +0800

    Fixed insert multi-query in survey

--- api/survey/create.php
+++ api/survey/create.php
@@ -12,19 +12,38 @@
 			$this->connection = $this->db->getConnection();
         }
 		
-        public function does_survey_exist($cust_id, $social_media_id, $others, $product_id, $buying_for_others, $ads_id)
+        public function does_survey_exist($cust_id, $social_media_id, $others, $product_id, $ads_id)
 		{
-            $query = "INSERT INTO customer_social_media (cust_id, social_media_id, others) 
-            VALUES ('$cust_id', '$social_media_id', '$others');";
 
-            $query .= "INSERT INTO customer_products (cust_id, product_id) 
-            VALUES ('$cust_id', '$product_id');";
+            $social_media_id = str_replace("[","", $social_media_id);
+            $social_media_id = str_replace("]","", $social_media_id);
+            $social_media_id = str_replace("","", $social_media_id);
+            $social_media_id = explode(",", $social_media_id);
+            $query = "";
+            foreach($social_media_id as $s_id) {
+                $query .= "INSERT INTO customer_social_media (cust_id, social_media_id, others) 
+                VALUES ('$cust_id', '$s_id', '$others');";
+            }
 
-            $query .= "UPDATE customers SET buying_for_others = '$buying_for_others' WHERE id = '$cust_id'";
-            
-            $query .= "INSERT INTO ads (cust_id, ads_id)
-            VALUES ('$cust_id', '$ads_id');";
-            
+            $ads_id = str_replace("[","", $ads_id);
+            $ads_id = str_replace("]","", $ads_id);
+            $ads_id = str_replace("","", $ads_id);
+            $ads_id = explode(",", $ads_id);
+
+            foreach($ads_id as $a_id) {
+                $query .= "INSERT INTO customer_ads (cust_id, ads_id)
+                VALUES ('$cust_id', '$a_id');";
+            }
+            $product_id = str_replace("[","", $product_id);
+            $product_id = str_replace("]","", $product_id);
+            $product_id = str_replace("","", $product_id);
+            $product_id = explode(",", $product_id);
+
+            foreach($product_id as $p_id) {
+                $query .= "INSERT INTO customer_products (cust_id, product_id) 
+                VALUES ('$cust_id', '$p_id');";
+            }
+
             $inserted = mysqli_multi_query($this->connection, $query);
             $json = array();
             if ($inserted == 1) {
@@ -36,22 +55,22 @@
             }
             echo json_encode($json);
             mysqli_close($this->connection);
+            
         }	
 	}
     
 	$user = new User();
-    if (isset($_POST['cust_id'], $_POST['social_media_id'], $_POST['others'], $_POST['product_id'], $_POST['buying_for_others'], $_POST['ads_id'])) {
+    if (isset($_POST['cust_id'], $_POST['social_media_id'], $_POST['others'], $_POST['product_id'], $_POST['ads_id'])) {
         $cust_id = $_POST['cust_id'];
         $social_media_id = $_POST['social_media_id'];
         $others = $_POST['others'];
         $product_id = $_POST['product_id'];
-        $buying_for_others = $_POST['buying_for_others'];
+        // $buying_for_others = $_POST['buying_for_others'];
         $ads_id = $_POST['ads_id'];
 
-		
-        if(!empty($cust_id) || !empty($social_media_id) || !empty($others) || !empty($product_id) || !empty($buying_for_others) || !empty($ads_id)){
+        if(!empty($cust_id) || !empty($social_media_id) || !empty($others) || !empty($product_id) || !empty($ads_id)){
 			
-			$user->does_survey_exist($cust_id, $social_media_id, $others, $product_id, $buying_for_others, $ads_id);
+			$user->does_survey_exist($cust_id, $social_media_id, $others, $product_id, $ads_id);
 		} 
 		
 		else {
